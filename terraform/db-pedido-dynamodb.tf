b9365a4510eaebcbc198aa3b00147a19962d8a80
rafa-fortes <r.s.neves@hotmail.com>
2025-02-08 20:52:49 -0300

    update files

--- terraform/db-pedido-dynamodb.tf
+++ terraform/db-pedido-dynamodb.tf
@@ -1,8 +1,8 @@
 ### DynamoDB para o serviço de pedidos ###
 resource "aws_dynamodb_table" "pedido_table" {
-  name           = "Pedidos"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "PedidoID"
+  name         = "Pedidos"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PedidoID"
 
   attribute {
     name = "PedidoID"
--- terraform/hpa.tf
+++ terraform/hpa.tf
@@ -6,23 +6,13 @@ resource "kubernetes_horizontal_pod_autoscaler" "pedido_hpa" {
 
   spec {
     scale_target_ref {
+      api_version = "apps/v1"
       kind        = "Deployment"
       name        = kubernetes_deployment.pedido-api.metadata[0].name
-      api_version = "apps/v1"
     }
 
-    min_replicas = 1
-    max_replicas = 10
-
-    metric {
-      type = "Resource"
-      resource {
-        name = "cpu"
-        target {
-          type                = "Utilization"
-          average_utilization = 50
-        }
-      }
-    }
+    min_replicas                      = 1
+    max_replicas                      = 10
+    target_cpu_utilization_percentage = 70
   }
 }
